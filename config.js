bddf3c1c807f3f54764f49baac1a830251221869
Christian Clausen <maxclausen@gmail.com>
2023-01-02 12:36:03 +0100

    Release version 1.2.8

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GIT_HOST = exports.SSH_HOST = exports.HTTP_HOST = void 0;
-exports.HTTP_HOST = "http://api.mist-cloud.io:30080";
-exports.SSH_HOST = "api.mist-cloud.io -p 30022";
-exports.GIT_HOST = "ssh://mist@api.mist-cloud.io:30022";
+exports.HTTP_HOST = "https://api.mist-cloud.io";
+exports.SSH_HOST = "api.mist-cloud.io";
+exports.GIT_HOST = "ssh://mist@api.mist-cloud.io";
